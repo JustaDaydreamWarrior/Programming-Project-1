8e7e61cf05192fe793a128dedaf02a81970f526d
Jeff JIANG <s3571902@student.rmit.edu.au>
2019-08-15 06:03:01 +1000

    appname, navbar, pageControl
    changed appname to Handshake.
    
    navbar button (Login&Register).
    
    changed index page's title to Homepage.
    
    linked navbar items to corresponding page.

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -6,26 +6,31 @@
           </div>
         </div>
         <nav class="navbar navbar-dark bg-dark">
-                <a class="navbar-brand" href="">Handshake - Job Matchmaking</a>
+                <a class="navbar-brand" href="/">Handshake - Job Matchmaking</a>
+                <form class="form-inline my-2 my-lg-0 ml-auto">
+                  <button type="button" class="btn btn-outline-primary" style="margin:5px;">Login</button>
+                  <button type="button" class="btn btn-outline-primary" style="margin-right:5px;">Register</button>
+                </form>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                   <span class="navbar-toggler-icon"></span>
                 </button>
                     <div class="collapse navbar-collapse" id="navbarNav">
                     <ul class="navbar-nav">
                         <li class="nav-item">
-                        <a class="nav-link" href="about">About</a>
+                        <a class="nav-link" href="/about">About</a>
                         </li>
                         <li class="nav-item">
-                        <a class="nav-link" href="services">Services</a>
+                        <a class="nav-link" href="/services">Services</a>
+                        </li>
+                        <!-- <li class="nav-item">
+                            <a class="nav-link" href="/services">Login</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="services">Login</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="services">Register</a>
-                        </li>
+                            <a class="nav-link" href="/services">Register</a>
+                        </li> -->
                     </ul>
                     </div>
+                  
           
         </nav>
       </div>
