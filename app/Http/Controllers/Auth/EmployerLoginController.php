9b6c9f2eb73edc47818d28d8ea93e88a0f5d2eaf
Jason Keen <s3591176@student.rmit.edu.au>
2019-09-04 15:16:42 +1000

    update EmployerLoginController

--- app/Http/Controllers/Auth/EmployerLoginController.php
+++ app/Http/Controllers/Auth/EmployerLoginController.php
@@ -7,5 +7,12 @@ use App\Http\Controllers\Controller;
 
 class EmployerLoginController extends Controller
 {
-    //
+    public function showLoginForm(){
+        return view('auth.employer-login');
+    }
+
+    public function login(){
+        return true;
+    }
+
 }
--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -30,4 +30,9 @@ class PagesController extends Controller
         $title = 'Registration';
         return view('pages/register')->with('title', $title);
     }
+
+    public function employer_login(){
+        $title = 'Employer Login';
+        return view('pages/Auth/employer-login')->with('title', $title);
+    }
 }
