c0acc57dc37bce1da7ad045a99a21562c02c2a2c
Jason Keen <s3591176@student.rmit.edu.au>
2019-09-04 15:16:14 +1000

    create employer-login view

---
+++ resources/views/auth/employer/employer-login.blade.php
@@ -0,0 +1,42 @@
+@extends('layouts.app')
+
+@section('title')
+    {{$title}}
+@endsection
+
+@section('content')
+    <br>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 col-md-offset-10">
+                <div class="panel panel-default">
+                    <div class="panel-heading"><strong>Employer Login</strong></div>
+
+                    <div class="panel-body" align="center">
+                        <p> Login now and begin your search! </p><br>
+                        <table>
+                            <tr>
+                                <td>Username:  </td>
+                                <td><input type="text" name="username"/></td>
+                            </tr>
+                            <tr>
+                                <td>Password:  </td>
+                                <td><input type="password" name="password"/></td>
+                            </tr>
+                            <tr>
+                                <td>&nbsp;</td>
+                                <td><input type="submit" value="Login"/></td>
+                            </tr>
+                        </table>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+@endsection
+
+
+
