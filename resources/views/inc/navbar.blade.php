3aa480f715489fce1bedd11f3783f7fc970427b7
Jason Keen <s3591176@student.rmit.edu.au>
2019-09-21 12:21:16 +1000

    Update navbar.blade.php
    temp fix

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -158,30 +158,6 @@
                                 <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
                             </li>
                         @endif
-//<<<<<< JoblistingExpansion
-//                   @else
-//                       <li class="nav-item dropdown">
-//                           <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-//                              data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-//                               {{ Auth::user()->name }} <span class="caret"></span>
-//                           </a>
-
-//                           <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-//                               <a class="dropdown-item" href='/jobPosts/create'>
-//                                   Create Job Listing
-//                               </a>
-
-//                               <a class="dropdown-item" href='/dashboard'>
-//                                   Dash Board
-//                               </a>
-//                               <a class="dropdown-item" href="{{ route('logout') }}"
-//                                  onclick="event.preventDefault();
-//                                                    document.getElementById('logout-form').submit();">
-//                                   {{ __('Logout') }}
-//                               </a>
-//======
-//>>>>>> develop
-
                     </ul>
                 </div>
             </div>
