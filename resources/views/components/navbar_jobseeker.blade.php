c710a631cba0c2167b85bd47e9ee89e1563c0b56
Anthony Pace <s3605187@student.rmit.edu.au>
2019-09-24 23:50:59 +1000

    update

--- resources/views/components/navbar_jobseeker.blade.php
+++ resources/views/components/navbar_jobseeker.blade.php
@@ -41,7 +41,7 @@
                                 <a class="nav-link" href="{{ route('support') }}">Support</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="{{ route('home') }}">Public Profiles</a>
+                                <a class="nav-link" href="{{ route('publicprofile') }}">Public Profiles</a>
                             </li>
 
                             <li class="nav-item">
