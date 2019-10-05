60a721dd0c79932b4cf839d41de18c690367be0a
Anthony Pace <s3605187@student.rmit.edu.au>
2019-10-06 00:54:49 +1000

    Update jobseekers profile page to display more

--- resources/views/jobseeker/profiles/jobseeker_profile_template.blade.php
+++ resources/views/jobseeker/profiles/jobseeker_profile_template.blade.php
@@ -40,71 +40,251 @@
                     <br>
                 </div>
                 <br>
+
+                <div>
+                    <h1>Education</h1>
+                    {!!$user->education!!}
+                    <br>
+                </div>
+                <br>
+
+                <div>
+                    <h1>Years Of Experience</h1>
+                    {!!$user->experience!!}
+                    <br>
+                </div>
+                <br>
                 <h1>Skills</h1>
                 <br>
-                <h1>Programming and Scripting Languages</h1>
+                <h5 align="center">Programming and Scripting Languages</h5>
 
                 <div class="checkbox-container">
-                    <div class="{{ $errors->has('bash') ? ' has-error' : '' }}">
+                    <div class="{{ $errors->has('') ? ' has-error' : '' }}">
                         <label for="bash" class="">Bash</label>
                         <br>
                         {!!$user->bash!!}
-
-                        <div class="{{ $errors->has('html') ? ' has-error' : '' }}">
-                            <label for="bash" class="">HTML</label>
-                            <br>
-                            {!!$user->html!!}
-                            <div class="{{ $errors->has('python') ? ' has-error' : '' }}">
-                                <label for="python" class="">Python</label>
-                                <br>
-                                {!!$user->python!!}
-                                <div class="{{ $errors->has('c') ? ' has-error' : '' }}">
-                                    <label for="c" class="">C</label>
-                                    <br>
-                                    {!!$user->c!!}
-                                    <div class="{{ $errors->has('java') ? ' has-error' : '' }}">
-                                        <label for="java" class="">Java</label>
-                                        <br>
-                                        {!!$user->java!!}
-                                        <div class="{{ $errors->has('ruby') ? ' has-error' : '' }}">
-                                            <label for="ruby" class="">Ruby</label>
-                                            <br>
-                                            {!!$user->ruby!!}
-                                            <div class="{{ $errors->has('c#') ? ' has-error' : '' }}">
-                                                <label for="c#" class="">c#</label>
-                                                <br>
-                                                {!!$user->c#!!}
-                                                <div class="{{ $errors->has('javascript') ? ' has-error' : '' }}">
-                                                    <label for="javascript" class="">Javascript</label>
-                                                    <br>
-                                                    {!!$user->javascript!!}
-                                                    <div class="{{ $errors->has('rust') ? ' has-error' : '' }}">
-                                                        <label for="rust" class="">Rust</label>
-                                                        <br>
-                                                        {!!$user->rust!!}
-                                                        <div class="{{ $errors->has('c++') ? ' has-error' : '' }}">
-                                                            <label for="c++" class="">C++</label>
-                                                            <br>
-                                                            {!!$user->c++!!}
-                                                            <div class="{{ $errors->has('powershell') ? ' has-error' : '' }}">
-                                                                <label for="powershell" class="">Powershell</label>
-                                                                <br>
-                                                                {!!$user->powershell!!}
-                                                                <div class="{{ $errors->has('sql') ? ' has-error' : '' }}">
-                                                                    <label for="sql" class="">SQL</label>
-                                                                    <br>
-                                                                    {!!$user->sql!!}
-                                                                    <div class="{{ $errors->has('css') ? ' has-error' : '' }}">
-                                                                        <label for="css" class="">CSS</label>
-                                                                        <br>
-                                                                        {!!$user->css!!}
-                                                                        <div class="{{ $errors->has('') ? ' has-error' : '' }}">
-                                                                            <label for="php" class="">PHP</label>
-                                                                            <br>
-                                                                            {!!$user->php!!}
-                                                                            <br>
-
+                        <br>
                     </div>
+
+                    <div class="{{ $errors->has('') ? ' has-error' : '' }}">
+                        <label for="html" class="">HTML</label>
+                        <br>
+                        {!!$user->html!!}
+                        <br>
+                    </div>
+
+                    <div class="{{ $errors->has('') ? ' has-error' : '' }}">
+                        <label for="python" class="">Python</label>
+                        <br>
+                        {!!$user->python!!}
+                        <br>
+                    </div>
+
+                    <div class="{{ $errors->has('') ? ' has-error' : '' }}">
+                        <label for="c" class="">C</label>
+                        <br>
+                        {!!$user->c!!}
+                        <br>
+                    </div>
+
+                    <div class="{{ $errors->has('') ? ' has-error' : '' }}">
+                        <label for="java" class="">Java</label>
+                        <br>
+                        {!!$user->java!!}
+                        <br>
+                    </div>
+
+                    <div class="{{ $errors->has('') ? ' has-error' : '' }}">
+                        <label for="ruby" class="">Ruby</label>
+                        <br>
+                        {!!$user->ruby!!}
+                        <br>
+                    </div>
+
+                    <div class="{{ $errors->has('') ? ' has-error' : '' }}">
+                        <label for="csharp" class="">C#</label>
+                        <br>
+                        {!!$user->csharp!!}
+                        <br>
+                    </div>
+
+                    <div class="{{ $errors->has('') ? ' has-error' : '' }}">
+                        <label for="javascript" class="">Javascript</label>
+                        <br>
+                        {!!$user->javascript!!}
+                        <br>
+                    </div>
+
+                    <div class="{{ $errors->has('') ? ' has-error' : '' }}">
+                        <label for="rust" class="">Rust</label>
+                        <br>
+                        {!!$user->rust!!}
+                        <br>
+                    </div>
+
+                    <div class="{{ $errors->has('') ? ' has-error' : '' }}">
+                        <label for="cplus" class="">C++</label>
+                        <br>
+                        {!!$user->cplus!!}
+                        <br>
+                    </div>
+
+                    <div class="{{ $errors->has('') ? ' has-error' : '' }}">
+                        <label for="powershell" class="">Powershell</label>
+                        <br>
+                        {!!$user->powershell!!}
+                        <br>
+                    </div>
+
+                    <div class="{{ $errors->has('') ? ' has-error' : '' }}">
+                        <label for="sql" class="">SQL</label>
+                        <br>
+                        {!!$user->sql!!}
+                        <br>
+                    </div>
+
+                    <div class="{{ $errors->has('') ? ' has-error' : '' }}">
+                        <label for="css" class="">CSS</label>
+                        <br>
+                        {!!$user->css!!}
+                        <br>
+                    </div>
+
+                    <div class="{{ $errors->has('') ? ' has-error' : '' }}">
+                        <label for="php" class="">PHP</label>
+                        <br>
+                        {!!$user->php!!}
+                        <br>
+                    </div>
+                        </div>
+
+                <hr>
+                <h5 align="center">Operating Systems</h5>
+
+                <div class="checkbox-container">
+                    <div class="{{ $errors->has('') ? ' has-error' : '' }}">
+                        <label for="linux" class="">Linux</label>
+                        <br>
+                        {!!$user->linux!!}
+                        <br>
+                    </div>
+
+
+                    <div class="{{ $errors->has('') ? ' has-error' : '' }}">
+                        <label for="windows7" class="">Windows 7</label>
+                        <br>
+                        {!!$user->windows7!!}
+                        <br>
+                    </div>
+
+                    <div class="{{ $errors->has('') ? ' has-error' : '' }}">
+                        <label for="iOS" class="">iOS</label>
+                        <br>
+                        {!!$user->iOS!!}
+                        <br>
+                    </div>
+
+                    <div class="{{ $errors->has('') ? ' has-error' : '' }}">
+                        <label for="macOS" class="">MacOS</label>
+                        <br>
+                        {!!$user->macOS!!}
+                        <br>
+                    </div>
+
+                    <div class="{{ $errors->has('') ? ' has-error' : '' }}">
+                        <label for="unix" class="">Unix</label>
+                        <br>
+                        {!!$user->unix!!}
+                        <br>
+                    </div>
+
+                    <div class="{{ $errors->has('') ? ' has-error' : '' }}">
+                        <label for="windowsOld" class="">Legacy Windows</label>
+                        <br>
+                        {!!$user->windowsOld!!}
+                        <br>
+                    </div>
+
+                    <div class="{{ $errors->has('') ? ' has-error' : '' }}">
+                        <label for="android" class="">Android</label>
+                        <br>
+                        {!!$user->android!!}
+                        <br>
+                    </div>
+
+                    <div class="{{ $errors->has('') ? ' has-error' : '' }}">
+                        <label for="windowsServer" class="">Windows Server</label>
+                        <br>
+                        {!!$user->windowsServer!!}
+                        <br>
+                    </div>
+
+                    <div class="{{ $errors->has('') ? ' has-error' : '' }}">
+                        <label for="windows10" class="">Windows 10</label>
+                        <br>
+                        {!!$user->windows10!!}
+                        <br>
+                    </div>
+                </div>
+                <hr>
+
+                <h5 align="center">Software</h5>
+
+                <div class="checkbox-container">
+
+                    <div class="{{ $errors->has('') ? ' has-error' : '' }}">
+                        <label for="microsoftOffice" class="">Microsoft Office</label>
+                        <br>
+                        {!!$user->microsoftOffice!!}
+                        <br>
+                    </div>
+
+                    <div class="{{ $errors->has('') ? ' has-error' : '' }}">
+                        <label for="adobe" class="">Creative Cloud</label>
+                        <br>
+                        {!!$user->adobe!!}
+                        <br>
+                    </div>
+
+
+
+                </div>
+
+                <hr>
+                <h5 align="center">Other Skills</h5>
+
+                <div class="checkbox-container">
+                    <div class="{{ $errors->has('') ? ' has-error' : '' }}">
+                        <label for="ciscoSystems" class="">Cisco Networking</label>
+                        <br>
+                        {!!$user->ciscoSystems!!}
+                        <br>
+                    </div>
+
+                    <div class="{{ $errors->has('') ? ' has-error' : '' }}">
+                        <label for="cloud" class="">Cloud Computing</label>
+                        <br>
+                        {!!$user->cloud!!}
+                        <br>
+                    </div>
+
+
+                </div>
+
+
+
+
+
+
+
+
+
+            </div>
+                </div>
+
+
+
+
 
 
     </div>
